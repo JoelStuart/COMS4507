b0af574bd00b06a77bd63086def5e5b0b1da1e91
JoelStuart <joel.stuart@uqconnect.edu.au>
2017-05-23 22:35:26 +1000

    MySQL fix 7

--- sendAddr.php
+++ sendAddr.php
@@ -9,7 +9,7 @@
 	$dbO->connect();
 	
     //$sql = "SELECT password FROM signup WHERE username = '".$username."'";
-	echo $addr;
+	echo json_encode($addr);
 	$sql = "INSERT INTO var (id, addr) VALUES(0, '$addr') ON DUPLICATE KEY UPDATE id=0, addr='$addr'";
 	$result = mysqli_query($dbO->link, $sql);
 	//	$result = mysqli_query($dbO->link, $sql)->fetch_object()->password;
