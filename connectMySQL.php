24063c3b3d42e162111afdb81d6ff602be8be4e7
JoelStuart <joel.stuart@uqconnect.edu.au>
2017-05-23 21:13:28 +1000

    Php test

--- connectMySQL.php
+++ connectMySQL.php
@@ -5,15 +5,21 @@ class MySQLDatabase
 
     function connect()
     {
-        $this->link = mysqli_connect('server-js.database.windows.net:1433', "stuuustuuu", "ComputerHorse1");
-        if (!$this->link) {
+		$connectionInfo = array("UID" => "stuuustuuu@server-js", "pwd" => "{your_password_here}", "Database" => "db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+		$serverName = "tcp:server-js.database.windows.net,1433";
+		$con = sqlsrv_connect($serverName, $connectionInfo);
+        //$this->link = mysqli_connect('server-js.database.windows.net:1433', "stuuustuuu", "ComputerHorse1");
+        /*if (!$this->link) {
             die('Not connected : ' . mysqli_error());
         }
         $db = mysqli_select_db($this->link, $database);
         if (!$db) {
             die ('Cannot use : ' . mysqli_error());
-        }
-        return $this->link;
+        }*/
+		if( $conn === false ) {
+		die( print_r( sqlsrv_errors(), true));
+}
+        return $conn;
     }
 
     function disconnect()
