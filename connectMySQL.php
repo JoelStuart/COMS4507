162be590d48be23c7b854a25260139cb10eac883
JoelStuart <joel.stuart@uqconnect.edu.au>
2017-05-23 21:19:46 +1000

    Php test 3

--- connectMySQL.php
+++ connectMySQL.php
@@ -5,14 +5,15 @@ class MySQLDatabase
 
     function connect()
     {
-        $this->link = mysqli_connect('server-js.database.windows.net:1433', "stuuustuuu", "ComputerHorse1", "db");
+        $this->link = mysqli_connect('server-js.database.windows.net:1433', 'stuuustuuu', 'ComputerHorse1');
         if (!$this->link) {
             die('Not connected : ' . mysqli_error());
         }
-        $db = mysqli_select_db($this->link, $database);
+        $db = mysqli_select_db($this->link, "db");
         if (!$db) {
             die ('Cannot use : ' . mysqli_error());
         }
+		echo('Connected to server');
         return $this->link;
     }
 
