2c0c399e23f13e62d79fa5aff26b34ea0fcd7757
Katie Deakin-Sharpe <katie.deakinsharpe@uqconnect.edu.au>
2017-05-24 17:08:33 +1000

    Fixed preferential+updated to include phases

--- js/ballot.sol
+++ js/ballot.sol
@@ -11,16 +11,37 @@ contract Ballot {
     bytes32[] public candidateList;
     mapping(address => Voter) public votersList;
     mapping (bytes32 => uint8) public votesForCandidates;
+    bytes32[] private winners;
+    bytes32[] private allWinners;
+    //phase 0: registration. phase 1: voting. phase 2: post election
+    uint8 private phase;
   
   function Ballot() {
+      phase = 0;
   }
   
   function addCandidate(bytes32 candidate) {
-      candidateList.push(candidate);
+      if (phase == 0) {
+        candidateList.push(candidate);
+      }
+  }
+  
+  function startVoting() {
+      phase = 1;
+  }
+  
+  function finishVoting() {
+      phase = 2;
+  }
+  
+  function getPhase() returns (uint8) {
+      return phase;
   }
   
   function addVoter(address voter) {
-      votersList[voter] = Voter({weight: 1, voted: false});
+      if (phase == 0) {
+        votersList[voter] = Voter({weight: 1, voted: false});
+      }
   }
 
   function totalVotesFor(bytes32 candidate) returns (uint8) {
@@ -29,10 +50,12 @@ contract Ballot {
   }
 
   function vote(bytes32 candidate) {
-    if (votersList[msg.sender].weight != 1 || votersList[msg.sender].voted) return;
-    if (validCandidate(candidate) == false) throw;
-    votesForCandidates[candidate] += 1;
-    votersList[msg.sender].voted = true;
+    if (phase == 1) {
+        if (votersList[msg.sender].weight != 1 || votersList[msg.sender].voted) return;
+        if (validCandidate(candidate) == false) throw;
+        votesForCandidates[candidate] += 1;
+        votersList[msg.sender].voted = true;
+    }
   }
 
   function validCandidate(bytes32 candidate) returns (bool) {
@@ -44,17 +67,26 @@ contract Ballot {
     return false;
   }
   
-  function getWinner() returns (bytes32) {
-      uint maxVotes = 0;
-      bytes32 winner = "";
+  function getWinner() returns (bytes32[]) {
+      if (phase == 2) {
+        uint maxVotes = 0;
+        for (uint i = 0; i < candidateList.length; i++) {
+            if (votesForCandidates[candidateList[i]] == maxVotes) {
+                allWinners.push(candidateList[i]);
+            } else if (votesForCandidates[candidateList[i]] > maxVotes) {
+                allWinners = new bytes32[](0);
+                maxVotes = votesForCandidates[candidateList[i]];
+                allWinners.push(candidateList[i]);
+            }
+        }
       
-      for (uint i = 0; i < candidateList.length; i++) {
-          if (votesForCandidates[candidateList[i]] > maxVotes) {
-              maxVotes = votesForCandidates[candidateList[i]];
-              winner = candidateList[i];
-          }
-      }
+        for (uint j = 0; j < allWinners.length; j++) {
+            if (allWinners[j] != "") {
+                winners.push(allWinners[j]);
+            } 
+        }
       
-      return winner;
+        return winners;
+      } 
   }
 }
