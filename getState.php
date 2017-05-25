59bc047f364c4f1a30268a7e0e877f3ed884e9c8
JoelStuart <joel.stuart@uqconnect.edu.au>
2017-05-25 14:46:25 +1000

    Updated contract in JS, added contract methods, added server side PHP and sender and getter methods in JS

---
+++ getState.php
@@ -0,0 +1,24 @@
+<?php
+
+
+session_start();
+
+	include('connectMySQL.php'); //make sure the path is correct.
+	$dbO = new MySQLDatabase(); //create a Database object
+	$dbO->connect();
+	
+    $sql = "SELECT addr FROM var WHERE id = 3";
+	//var query = "INSERT INTO var (id, addr) VALUES(0, '".$addr."') ON DUPLICATE KEY UPDATE";
+	$result = mysqli_query($dbO->link, $sql);
+	//	$result = mysqli_query($dbO->link, $sql)->fetch_object()->password;
+    //$result = $conn->query($sql);
+	if (!$result){
+		die('Could not query:' . mysql_error());
+	}
+	
+	$ret = $result->fetch_object()->addr;
+	echo $ret;
+    $dbO->disconnect();
+	
+
+?>
